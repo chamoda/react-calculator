c251f45fcfe9b62b2526c7144de29131e352d638
Chamoda Pandithage <chamoda@Chamodas-MacBook-Pro.local>
2019-05-04 19:45:26 +0530

    Updates

---
+++ precache-manifest.f82cbde42be7c9086df6e9746127b32a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cafc3e039b4c9eba0ada2ee6de8a308",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "435e7eecde75e3896ec5",
+    "url": "/react-calculator/static/css/main.45defcba.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/react-calculator/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "435e7eecde75e3896ec5",
+    "url": "/react-calculator/static/js/main.abb8a90a.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
